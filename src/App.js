2cadfd111dda52f22c1c4abf7ca3bbf01acdd8b6
Rafael Azevedo <74427905+RafaelBiatoAzevedo@users.noreply.github.com>
2021-03-09 17:41:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Search from './services/Search';
 import List from './services/List';
 
 function App() {
-  api.getCategories().then((categories) => console.log(categories));
   return (
     <div className="App">
       <Search />
