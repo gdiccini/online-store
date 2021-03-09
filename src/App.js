18a5864ba29adf76086486b76e7876976f2ca690
Bernardo Smith <bernardostech@gmail.com>
2021-03-09 15:10:39 -0500

    questao 2 respondida

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import * as api from './services/api';
+import Search from './services/Search';
+import List from './services/List';
 
 function App() {
   api.getCategories().then((categories) => console.log(categories));
-
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <Search />
+      <List />
     </div>
   );
 }
