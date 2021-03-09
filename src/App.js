f023b994f6a69615ab36380a185ec7401036fc26
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-03-09 16:25:03 -0300

    fix eslint2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as api from './services/api'
+import * as api from './services/api';
 
 function App() {
-  api.getCategories().then(categories => { console.log(categories) })
+  api.getCategories().then((categories) => console.log(categories));
 
   return (
     <div className="App">
-
+      <h1>Teste</h1>
     </div>
   );
 }
